ee0ec801e228f3e8697e90d30615857e79a2b405
Justyna-JustCode <justyna.justcode@gmail.com>
2021-08-21 11:09:16 +0200

    #23: Updates variables script for linux delivery creation to support newest LTS Qt package and newest qt installer framework

--- scripts/desktop/linux/variables.sh
+++ scripts/desktop/linux/variables.sh
@@ -7,10 +7,10 @@ PROJECT_DIR=$(readlink -m "${SCRIPTS_DIR}/../../../")
 # setup up this with your variables ==================
 # QT:
 QT_PATH="/opt/Qt/"
-QT_VERSION="5.12.5"
+QT_VERSION="5.15.2"
 QT_COMPILER="gcc_64"
 LINUXDEPLOYQT_FILE="${QT_PATH}/Tools/linuxdeployqt.AppImage"
-QT_INSTALLER_FRAMEWORK_BIN="${QT_PATH}/Tools/QtInstallerFramework/3.1/bin/"
+QT_INSTALLER_FRAMEWORK_BIN="${QT_PATH}/Tools/QtInstallerFramework/4.1/bin/"
 # ====================================================
 
 QMAKE_FILE=$(readlink -m "/$QT_PATH/$QT_VERSION/$QT_COMPILER/bin/qmake")
