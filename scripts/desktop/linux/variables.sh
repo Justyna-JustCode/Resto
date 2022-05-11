b699bc07296f3a9aa4f0314cc8d1874b7e801d9f
Justyna-JustCode <justyna.justcode@gmail.com>
2022-05-11 09:14:56 +0200

    #27: GitHub actions delivery - attempt no7.

--- scripts/desktop/linux/variables.sh
+++ scripts/desktop/linux/variables.sh
@@ -6,11 +6,8 @@ PROJECT_DIR=$(readlink -m "${SCRIPTS_DIR}/../../../")
 
 # setup up this with your variables ==================
 # QT:
-QT_PATH="/opt/Qt/"
-QT_VERSION="5.12.5"
-QT_COMPILER="gcc_64"
-LINUXDEPLOYQT_FILE="${QT_PATH}/Tools/linuxdeployqt.AppImage"
-QT_INSTALLER_FRAMEWORK_BIN="${QT_PATH}/Tools/QtInstallerFramework/3.1/bin/"
+LINUXDEPLOYQT_FILE="${QT_PATH}/Tools/inuxdeployqt-continuous-x86_64.AppImage"
+QT_INSTALLER_FRAMEWORK_BIN="${QT_PATH}/Tools/QtInstallerFramework/${QT_IFW_VERSION}/bin/"
 # ====================================================
 
 QMAKE_FILE=$(readlink -m "/$QT_PATH/$QT_VERSION/$QT_COMPILER/bin/qmake")
@@ -19,7 +16,7 @@ QMAKE_FILE=$(readlink -m "/$QT_PATH/$QT_VERSION/$QT_COMPILER/bin/qmake")
 ORG_NAME="JustCode"
 ORG_URL="http://just-code.org"
 APP_NAME="Resto"
-APP_DESC="A small application for work time management"
+APP_DESC="Resto is a simple application to control work time and breaks."
 APP_URL="${ORG_URL}/applications/${APP_NAME,,}"
 APP_CATEGORIES="Utility;Office;"
 # ====================================================
