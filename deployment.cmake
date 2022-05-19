be66edc6cdfb28d770c22e1740f7f73efb739be0
Justyna-JustCode <justyna.justcode@gmail.com>
2022-05-19 20:12:41 +0200

    #26: Monolith call improvement.

--- deployment.cmake
+++ deployment.cmake
@@ -3,5 +3,5 @@ OPTION(DEPLOY_HELP "Build an application for production." ON)
 
 if (DEPLOY_HELP)
     set(HTML2STANDALONE "monolith")
-    add_custom_command(TARGET Resto POST_BUILD COMMAND ${HTML2STANDALONE} ${CMAKE_SOURCE_DIR}/help/help.html -o ${CMAKE_BINARY_DIR}/help.html)
+    add_custom_command(TARGET Resto POST_BUILD COMMAND ${HTML2STANDALONE} file:${CMAKE_SOURCE_DIR}/help/help.html -o ${CMAKE_BINARY_DIR}/help.html)
 endif()
