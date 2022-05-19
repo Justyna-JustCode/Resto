52d8e210abbcd287326d969345da0e21a2804a78
Justyna-JustCode <justyna.justcode@gmail.com>
2022-05-19 16:55:40 +0200

    #26: Deploy resources together with help + html improvements

--- deployment.cmake
+++ deployment.cmake
@@ -1,4 +1,7 @@
 # deploy help data
 add_custom_command(TARGET Resto POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/help/help.html ${CMAKE_BINARY_DIR}
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_SOURCE_DIR}/help/helpResources ${CMAKE_BINARY_DIR}/helpResources
+)
+add_custom_command(TARGET Resto POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/help/* ${CMAKE_BINARY_DIR}
 )
